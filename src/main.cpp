e3768b94e4df00ccacbeb57e9b3daf2d5a761d8e
panagiotou23 <alexandp@ece.auth.gr>
2021-02-20 06:01:53 +0200

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,72 @@
 #include <iostream>
 #include <vector>
 #include <filtering.hpp>
+#include <cstdlib>
+#include <string> 
 
-int main()
+int main(int argc, char** argv)
 {   
     std::cout << std::endl;
 
 /* ------------------------- house image parameters ------------------------- */
 
     int n = 64;
-    int patchSize = 5;
-    double filterSigma = 0.06;
-    double patchSigma = 0.8;
-    // double filterSigma = 0.02;
-    // double patchSigma = 5/3;
+    int patchSize;
+    double filterSigma;
+    double patchSigma;
+    if(argc == 1){
+        patchSize = 5;
+        filterSigma = 0.03;
+        patchSigma = 0.7;
+    }else if(argc == 4){
+        patchSize = atoi(argv[1]);
+        filterSigma = atof(argv[2]);
+        patchSigma = atof(argv[3]);
+    }else{
+        return 1;
+    }
+
+    // std::cout << patchSize << "\t" << filterSigma << "\t" << patchSigma << std::endl;
 
 /* ------------------------------ file reading ------------------------------ */
 
     std::vector<double> image(n * n);
     image = file::read("./noisy_house.txt", n, n);
 
-    // prt::rowMajorVector(image, n, n);
+    std::cout << "Read the image" << std::endl;
 
 /* ----------------------------- image filtering ---------------------------- */
 
     std::vector<double> filteredImage = filterImage(image, n, patchSize, patchSigma, filterSigma);
-    
-    // std::cout << "filtered image:\n\n";
-    // prt::rowMajorVector(filteredImage, n, n);
+
+    std::cout << "Filtered the image" << std::endl;
+
+    std::vector<double> res(n *n);
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            res[i * n + j] = filteredImage[i * n + j] - image[i * n + j];
+        }
+    }
+
+    std::cout << "Calculated the residual" << std::endl;
 
 /* ------------------------------ file writing ------------------------------ */
 
-    file::write(filteredImage, "filtered_image", n, n);
+    std::string dataPath = "data/";
+    std::string filteredPath = dataPath + "filtered_image_" + 
+                                std::to_string(patchSize) + "_" + 
+                                std::to_string(filterSigma) + "_" + 
+                                std::to_string(patchSize); 
+    file::write(filteredImage, filteredPath, n, n);
+
+
+    std::string resPath = dataPath + "residual_" + 
+                                std::to_string(patchSize) + "_" + 
+                                std::to_string(filterSigma) + "_" + 
+                                std::to_string(patchSize); 
+    file::write(res, resPath, n, n);
+
+    std::cout << "Wrote the filtered image and the residual" << std::endl;
 
 /* -------------------------------------------------------------------------- */
 
