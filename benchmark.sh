8287ad435a98680ec1baa14d115b47df0925a6b4
georgegito <gitopoulos99@gmail.com>
2021-02-25 16:59:55 +0200

    Use loop for sigmas in the script

--- benchmark.sh
+++ benchmark.sh
@@ -3,24 +3,30 @@
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=1:00
+#SBATCH --time=2:00
 
 module load gcc
 module load cuda
 nvidia-smi
 
 #imageNum={house:0, flower:1, lena:2}
-imageNum=0
+imageNum=1
 
 #parameters
 patchSize=5
-filterSigma=0.06
-patchSigma=0.8
-useGpu=0
-useSharedMem=0
+filterSigma=(0.04 0.06 0.08 0.1 0.12 0.14)
+patchSigma=(0.4 0.6 0.8 1 1.2 1.4)
+useGpu=1
+useSharedMem=1
 
 #compile
 nvcc -o build/main -I./include src/main.cu -O3
 
-#run
-./build/main $imageNum $patchSize $filterSigma $patchSigma $useGpu $useSharedMem
+for i in ${filterSigma[@]}; do
+    for j in ${patchSigma[@]}; do
+        #run
+        ./build/main $imageNum $patchSize $i $j $useGpu $useSharedMem 
+    done
+done
+
+
